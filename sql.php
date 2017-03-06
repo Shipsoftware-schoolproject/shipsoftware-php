3d5c8d38f4ad8065d1fb050c8af6a7fe6c72932b
Lihis <lihis@lihis.net>
2017-03-06 09:58:44 +0200

    Paranneltu mysql tunnareita

--- sql.php
+++ sql.php
@@ -1,25 +1,26 @@
 <?php
 
-$tunnarit = array();
-
 # Tiedostoon ekalle riville SQL käyttäjänimi ja toiselle riville salasana
-$fd = fopen("../../mysql_tunnarit.tx", "r");
-if ($fd) {
-	$i = 0;
-	while (($line = fgets($fd)) !== false) {
-		$tunnarit[$i] = $line;
-		$i++;
-	}
-} else {
-	echo "MySQL tunnuksien lataaminen ei onnistunut.";
-	http_response_code(500);
+$tunnarit = file("../../mysql_tunnarit.txt", FILE_IGNORE_NEW_LINES);
+if ($fd === false) {
+	display_error("MySQL tunnuksien lataaminen ei onnistunut.");
+}
+
+if (count($tunnarit)) != 2) {
+	display_error("MySQL tiedoston sisältö ei ole validi");
+}
+
+function display_error($message, $error_code = 500)
+{
+	echo $message;
+	http_response_code($error_code);
 	die();
 }
 
 $conn = mysqli_connect("mysql.cc.puv.fi", $tunnarit[0], $tunnarit[1], $tunnarit[0] . "_Kurssi");
 
-if (!$conn ) {
-    echo "Connection could not be established.<br />";
+if (!$conn) {
+    display_error("Tietokanta yhteyden muodostaminen epäonnistui");
 }
 
 ?>
