91b7c9fab9686a405045b875ca3990f5267fa002
Jori <e1501117@edu.vamk.fi>
2018-02-20 11:27:45 +0200

    UpdatedTime shipjs

--- public/js/ship.js
+++ public/js/ship.js
@@ -29,7 +29,7 @@ function get_ships(data = null)
 
             if (ships[i]['Lat'] !== null) {
                 addMarker(ships[i]['IMO'], ships[i]['Lat'], ships[i]['Lng'],
-                    ships[i]['ShipName']);
+                    ships[i]['ShipName'],ships[i]['UpdatedTime']);
             }
         }
 
