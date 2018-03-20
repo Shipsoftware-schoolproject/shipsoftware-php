4153284f833a34e6f060375e3a34ce6ef50d3b85
Lihis <lihis@lihis.net>
2018-03-20 13:44:01 +0200

    Persons table is now Users

--- app/User.php
+++ app/User.php
@@ -14,7 +14,7 @@ class User extends Authenticatable
      *
      * @var string
      */
-    protected $table = 'Persons';
+    protected $table = 'Users';
 
     /**
      * Users table primary key
