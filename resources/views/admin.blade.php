8cf6fdc34efa80439f95daecb6839dad3363ae80
Lihis <lihis@lihis.net>
2018-03-28 11:21:17 +0300

    Removed comment from admin.blade.php

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -25,7 +25,6 @@
     <div class="col-lg-9">
         <!-- Tabs content -->
         <div class="tab-content">
-            <!-- Ship info tab -->
             @include('admin.users')
         </div>
     </div>
