7650ed08d8441b5a6f9171dd53aac3fd7b2989f9
Lihis <lihis@lihis.net>
2018-03-28 11:17:55 +0300

    Initial work on admin panel

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -15,12 +15,18 @@
                 <ul class="nav nav-pills nav-stacked" role="tablist">
                     <li class="active"><a href="#shipinfo" role="tab" data-toggle="tab">{{ trans('ship.info') }}</a></li>
 					<li><a href="#editship" role="tab" data-toggle="tab">Laivat</a></li>
-                    <li><a href="#editcrew" role="tab" data-toggle="tab">Miehistö</a></li>
+                    <li><a href="#editcrew" role="tab" data-toggle="tab">MiehistÃ¶</a></li>
                     <li><a href="#editcargo" role="tab" data-toggle="tab">Rahti</a></li>
-					<li><a href="#editcompany" role="tab" data-toggle="tab">Yhtiö</a></li>
+					<li><a href="#editcompany" role="tab" data-toggle="tab">YhtiÃ¶</a></li>
                 </ul>
             </div>
         </div>
-        <a href="{{ url('/') }}"><input class="btn btn-primary" type="button" value="{{ trans('misc.back_to_map') }}" /></a>
+    </div>
+    <div class="col-lg-9">
+        <!-- Tabs content -->
+        <div class="tab-content">
+            <!-- Ship info tab -->
+            @include('admin.users')
+        </div>
     </div>
 @endsection
---
+++ resources/views/admin/users.blade.php
@@ -0,0 +1,5 @@
+<div class="tab-pane active" id="cargo">
+    <div class="col-lg-12">
+        <p>Hello world</p>
+    </div>
+</div>
