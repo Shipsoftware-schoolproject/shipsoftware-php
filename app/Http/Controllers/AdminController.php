3f314dd3826537ae755dfa878a8d94aa7b557169
Tomi Lähteenmäki <lihis@lihis.net>
2018-03-29 11:20:53 +0300

    Initial Modal for adding/editing users

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,17 +6,20 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 use App\User;
+use App\Role;
+use App\Company;
 
 class AdminController extends Controller
 {
     public function index()
     {
         if (!Auth::User()->isAdmin()) {
+            // FIXME: Return some nice view..
             return 'You\'re not an Admin';
         }
 
         $users = User::with(['role', 'company'])->get();
 
-        return view('admin')->with(['users' => $users]);
+        return view('admin')->with(['users' => $users, 'roles' => Role::all(), 'companies' => Company::all()]);
     }
 }
--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -1,4 +1,40 @@
-<div class="tab-pane" id="users">
+@section('scripts')
+    <script>
+        function setModalTitle(title) {
+            $('#userModalTitle').text(title);
+        }
+
+        function showModal() {
+            $('#userModal').modal('show');
+        }
+
+        function clearModal()
+        {
+            $('#firstName').val('');
+            $('#lastName').val('');
+            $('#phoneNumber').val('');
+            $('#email').val('');
+            $('#username').val('');
+            $('#password').val('');
+        }
+
+        function addUser() {
+            setModalTitle('Lisää käyttäjä');
+            clearModal();
+
+            showModal();
+        }
+
+        function editUser(id) {
+            setModalTitle('Muokkaa käyttäjää');
+            clearModal();
+
+            showModal();
+        }
+    </script>
+@endsection
+
+<div class="tab-pane active" id="users">
     <div class="col-lg-12">
         <div class="panel panel-default">
             <div class="panel-heading">
@@ -23,10 +59,10 @@
                         <td>{{ $user->role ? $user->role->Name : '-' }}</td>
                         <td>{{ $user->company ? $user->company->Name : '-' }}</td>
                         <td>
-                            <button type="button" class="btn btn-sm btn-warning">
+                            <button type="button" class="btn btn-sm btn-warning" onclick="editUser({{ $user->ID }});">
                                 <span class="glyphicon glyphicon-pencil"></span>
                             </button>
-                            <button type="button" class="btn btn-sm btn-danger">
+                            <button type="button" class="btn btn-sm btn-danger" onclick="deleteUser({{ $user->ID }});">
                                 <span class="glyphicon glyphicon-trash"></span>
                             </button>
                         </td>
@@ -35,9 +71,118 @@
             </table>
         </div>
         <div>
-            <button type="button" class="btn btn-success" id="addUser">Lisää käyttäjä</button>
-            <button type="button" class="btn btn-warning" id="editUser">Muokkaa käyttäjää</button>
-            <button type="button" class="btn btn-danger" id="deleteUser">Poista käyttäjä</button>
+            <button type="button" class="btn btn-success" onclick="addUser();">Lisää käyttäjä</button>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="userModal" tabindex="-1" role="dialog" aria-labelledby="userModal">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="userModalTitle"></h4>
+            </div>
+            <div class="modal-body">
+                <form id="userForm">
+                    <div class="row">
+                        <div class="col-sm-4">
+                            <div class="form-group">
+                                <label for="firstName">Etunimi:</label>
+                                <input type="text" class="form-control" name="firstName" id="firstName" placeholder="Erkki">
+                                <span id="helpFirstName" class="help-block hidden"></span>
+                            </div>
+                        </div>
+                        <div class="col-sm-5">
+                            <div class="form-group">
+                                <label for="lastName">Sukunimi:</label>
+                                <input type="text" class="form-control" name="lastName" id="lastName" placeholder="Esimerkki">
+                                <span id="helpLastName" class="help-block hidden"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-4">
+                            <div class="form-group">
+                                <label for="phoneNumber">Puhelinnumero:</label>
+                                <input type="text" class="form-control" name="phoneNumber" id="phoneNumber" placeholder="0401234567">
+                                <span id="helpPhoneNumber" class="help-block hidden"></span>
+                            </div>
+                        </div>
+                        <div class="col-sm-5">
+                            <div class="form-group">
+                                <label for="email">Email:</label>
+                                <input type="text" class="form-control" name="email" id="email" placeholder="erkki.esimerkki@example.com">
+                                <span id="helpEmail" class="help-block hidden"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-4">
+                            <div class="form-group">
+                                <label for="username">Käyttäjänimi:</label>
+                                <input type="text" class="form-control" name="username" id="username" placeholder="eres">
+                                <span id="helpUsername" class="help-block hidden"></span>
+                            </div>
+                        </div>
+                        <div class="col-sm-5">
+                            <label for="password">Salasana:</label>
+                            <div class="input-group">
+                                <input type="password" class="form-control" name="password" id="password">
+                                <div class="input-group-btn">
+                                    <button type="button" class="btn btn-default">Generoi</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-4">
+                            <div class="form-group">
+                                <label for="userPicture">Kuva:</label>
+                                <!-- FIXME: Find good default picture and show
+                                            that if user doesn't have a profile
+                                            picture
+                                -->
+                                <img style="width: 100%;" src="" id="userPicture">
+                                <input type="file" style="padding-top: 1em;" id="picture" name="picture">
+                                <span id="helpPicture" class="help-block hidden"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-4">
+                            <div class="form-group">
+                                <label for="role">Rooli:</label>
+                                <select id="role" class="form-control">
+                                    <option value=""></option>
+                                    @foreach($roles as $role)
+                                        <option value="{{ $role->ID }}">{{ $role->Name }}</option>
+                                    @endforeach
+                                </select>
+                                <span id="helpRole" class="help-block hidden"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-4">
+                            <div class="form-group">
+                                <label for="company">Yhtiö:</label>
+                                <select id="company" class="form-control">
+                                    <option value=""></option>
+                                    @foreach($companies as $company)
+                                        <option value="{{ $company->ID }}">{{ $company->Name }}</option>
+                                    @endforeach
+                                </select>
+                                <span id="helpCompany" class="help-block hidden"></span>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Sulje</button>
+                <button type="button" class="btn btn-primary">Tallenna</button>
+            </div>
         </div>
     </div>
 </div>
